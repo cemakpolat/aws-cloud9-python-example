809619d31a5297b23e22541ec3b98b80b9d805e9
EC2 Default User <ec2-user@ip-172-31-10-8.ec2.internal>
2022-09-22 12:46:35 +0000

    code containing error

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def sub(p1, p2):
 
 if __name__ == "__main__":
     val1 = 10
-    val2 = 5
+    val2 = 
     result = add(val1, val2)
     print(f"{val1}+{val2} is equal to {result}")
     result = sub(val1, val2)
